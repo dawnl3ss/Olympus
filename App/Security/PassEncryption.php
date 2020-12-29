cb52288fb89503f2db945db182aa4df341884229
Neptune-Dev <52249849+Neptune-Dev@users.noreply.github.com>
2020-12-29 23:47:37 +0100

    New encrypting system started

---
+++ App/Security/PassEncryption.php
@@ -0,0 +1,37 @@
+<?php
+
+class PassEncryption {
+
+    /**
+     * @param $plaintext
+     *
+     * @param $password
+     *
+     * @return string
+     */
+    public static function encrypt_pass($plaintext, $password) {
+        $method = "AES-256-CBC";
+        $key = hash('sha256', $password, true);
+        $iv = openssl_random_pseudo_bytes(16);
+        $ciphertext = openssl_encrypt($plaintext, $method, $key, OPENSSL_RAW_DATA, $iv);
+        $hash = hash_hmac('sha256', $ciphertext . $iv, $key, true);
+        return $iv . $hash . $ciphertext;
+    }
+
+    /**
+     * @param $ivHashCiphertext
+     *
+     * @param $password
+     *
+     * @return false|string|null
+     */
+    public static function decrypt_pass($ivHashCiphertext, $password) {
+        $method = "AES-256-CBC";
+        $iv = substr($ivHashCiphertext, 0, 16);
+        $hash = substr($ivHashCiphertext, 16, 32);
+        $ciphertext = substr($ivHashCiphertext, 48);
+        $key = hash('sha256', $password, true);
+        if (!hash_equals(hash_hmac('sha256', $ciphertext . $iv, $key, true), $hash)) return null;
+        return openssl_decrypt($ciphertext, $method, $key, OPENSSL_RAW_DATA, $iv);
+    }
+}
